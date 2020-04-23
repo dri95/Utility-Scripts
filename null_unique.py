89df27dd9ca87a7a8391198934294ad389aba9af
dri95 <44446376+dri95@users.noreply.github.com>
2020-04-23 13:43:25 -0400

    Add files via upload

---
+++ null_unique.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv(".....csv")
+
+def miss_val(data):
+    if dataset.isnull().values.any():
+       miss = (data.isnull().sum().sort_values(ascending=False))
+       return miss
+    else: print('There are no missing values in the Dataset')
+    
+    
+miss_val(dataset)
+
+# num -> is the number of unique observations to be displayed
+
+def unique(data,num):
+    for c in list(data.columns):
+        n = data[c].unique()
+        if len(n)<= num:
+           print(c)
+           print(n)
+        else:
+           print(c + ': ' +str(len(n)) + ' unique values')
+
+unique(dataset,5)
+
+
